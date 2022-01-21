d6f6545cd937e664f63c218e733ba4674a195798
haregewin <haregamba@gmail.com>
2022-01-21 16:31:21 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MovieCard from "./Components/MovieCard/MovieCard";
+import MovieDetail from "./Components/MovieDetail/MovieDetail";
+import { getMovieDetailsById, getMoviesBySearchTerm } from "./utils/Utils";
 
 function App() {
+  let movieData = {};
+  let movieDataa = {};
+  let isMovieData = false;
+  let isMovieDataa = false;
+
+  window.onload = async () => {
+    const movieAPI = await getMovieDetailsById("tt2975590");
+    sessionStorage.setItem("movieData", JSON.stringify(movieAPI));
+  };
+  if (sessionStorage.getItem("movieData")) {
+    movieData = JSON.parse(sessionStorage.getItem("movieData"));
+    isMovieData = true;
+  }
+ window.onload = async () => {
+    const movieAPITerm = await getMoviesBySearchTerm("spiderman");
+    sessionStorage.setItem("movieDataa", JSON.stringify(movieAPITerm));
+  };
+  if (sessionStorage.getItem("movieDataa" )) {
+    movieDataa = JSON.parse(sessionStorage.getItem("movieDataa"));
+    isMovieDataa = true;
+  }
+
+  console.log(movieData);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isMovieData ? (
+        <MovieCard
+          title={movieData.Title}
+          type={movieData.Type}
+          posterUrl={movieData.Poster}
+        />
+      ) : (
+        "Please refresh"
+      )}
+      {isMovieData ? (
+        <MovieDetail
+          title={movieData.Title}
+          rated={movieData.Type}
+          posterUrl={movieData.Poster}
+          runtime={movieData.Runtime}
+          genre={movieData.Genre}
+          plot={movieData.Plot}
+          actors={movieData.Actors}
+          rating={movieData.imdbRating}
+          rated={movieData.Rated}
+        />
+      ) : (
+        "Please refresh"
+      )}
+
     </div>
   );
 }
---
+++ src/Components/MovieList.js
@@ -0,0 +1,22 @@
+
+   
+// import React from "react";
+// import MovieCard from "../MovieCard";
+
+// function MovieList({ movies }) {
+//   console.log(movies);
+
+//   return (
+//     <div>
+//       {movies.map((movie) => (
+//         <div key={movie.imdbID}>
+//           title={movie.Title}
+//           posterUrl={movie.Poster}
+//           type={movie.Type}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default MovieList;
